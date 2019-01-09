545634b789a50154e842f45c5b767d18d83cf829
Xiaochen Li <jacobxiaochen@sina.com>
2019-01-08 18:39:49 -0800

    yyzhou's webpage update

--- Main.java
+++ Main.java
@@ -3,7 +3,8 @@ public class Main {
 
   public static void main(String[] args) {
 
-    String url = "https://seuperhakkerja.github.io";
+    String url = "https://cseweb.ucsd.edu/~yyzhou/" ;
+  
 
     Bot bot = new Bot(url);
     bot.start();
